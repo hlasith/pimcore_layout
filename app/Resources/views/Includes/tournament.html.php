466fe6fc7d0bb2261853a2d14214694ded003ec2
hlasith <hlasith@yahoo.com>
2018-03-15 15:05:12 +0100

    fix

--- app/Resources/views/Includes/tournament.html.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @var \Pimcore\Templating\PhpEngine $this
- * @var \Pimcore\Templating\PhpEngine $view
- * @var \Pimcore\Templating\GlobalVariables $app
- */
-?>
-<script>window.Tether = {};</script>
-<?= $this->areablock('content'); ?>
